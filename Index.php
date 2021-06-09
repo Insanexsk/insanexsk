bf4c922d68e10d1f5a4e54cfb1a1dd462955e3ca
Insanexsk <Leo_574@live.com.mx>
2021-06-09 13:19:44 -0500

    beta1

---
+++ Index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Inicio</title>
+    <script src="https://kit.fontawesome.com/a72d96f673.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/Estilos.css">
+</head>
+<body>
+
+<!--- Menu de navegación-->
+    <header>
+        <div class="header-content">   
+        <div class="logo">
+                <img src="takablogov1.png" alt="logo" width="80" height="70">
+        </div>
+        <div class="menu" id="show-menu">
+            <nav>
+                <ul>
+                    <li><a href="#"><i class="fas fa-home"></i> Inicio</a></li>
+                    <li><a href="#"><i class="fas fa-boxes"></i> Productos</a></li>
+                    <li><a href="#"><i class="fas fa-bolt"></i> Servicios</a></li>
+                    <li><a href="#"><i class="fas fa-headset"></i> Contáctanos</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <div id="icon-menu">
+            <i class="fas fa-bars"></i>
+        </div>
+
+        </div>
+    </header>
+
+<!--- Portada -->
+    <div class="container-all" id="move-content">
+    <div class="container-cover">
+        <div class="container-cover-info">
+        
+            <h1>Bienvenido a Takab</h1>
+            <p>Lo mejor lo estamos creando.</p>
+        </div>
+    </div>
+
+<!--- Cuerpo de la pagina-->
+    <div class="container-content">
+        <article>
+            <h1>Entrada 1</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Beatae consequuntur cum, 
+                odio iusto aperiam voluptatibus. Aliquid, 
+                asperiores vero distinctio accusamus quis 
+                nisi animi magni recusandae vitae inventore 
+                perspiciatis, sunt quasi?
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Incidunt voluptas 
+                repudiandae dolores at! Ad rerum eum 
+                asperiores perspiciatis molestias eligendi 
+                minus itaque repudiandae atque blanditiis 
+                recusandae laboriosam, eius ipsam? Autem.
+            </p>
+            <img src="catalina.jpg">
+        </article>
+
+        <article>
+            <h1>Entrada 2</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Beatae consequuntur cum, 
+                odio iusto aperiam voluptatibus. Aliquid, 
+                asperiores vero distinctio accusamus quis 
+                nisi animi magni recusandae vitae inventore 
+                perspiciatis, sunt quasi?
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Incidunt voluptas 
+                repudiandae dolores at! Ad rerum eum 
+                asperiores perspiciatis molestias eligendi 
+                minus itaque repudiandae atque blanditiis 
+                recusandae laboriosam, eius ipsam? Autem.
+            </p>
+            <img src="catalina.jpg">
+        </article>
+
+    </div>
+
+
+<!---Pie de página-->
+    <div class="container-footer">
+        <footer>
+            <!---<div class="logo-footer">
+                <img src="takablogo.png" alt="">
+            </div>-->
+
+            <div class="redes-footer">
+                <a href="https://www.facebook.com/takabtechnology"><i class="fab fa-facebook-f icon-redes-footer"></i></a>
+                <a href="https://twitter.com/TakabTechnology"><i class="fab fa-twitter icon-redes-footer"></i></a>
+                <a href="https://www.instagram.com/takabtechnology"><i class="fab fa-instagram icon-redes-footer"></i></a>
+                <a href="https://www.youtube.com/channel/UCbQYQrEaj4sTaTMHX-OWqcw"><i class="fab fa-youtube icon-redes-footer"></i></a>
+            </div>
+
+            <hr>
+            <h4>® 2021 Takab Technology - Todos los derechos reservados.</h4>
+        </footer>
+    </div>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+
+
+</html>
